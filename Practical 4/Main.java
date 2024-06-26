4de6e2a047a09d9fb4b38f40572baaa54d14c4bc
Princelad <ladprince1112@gmail.com>
2024-06-26 15:59:02 +0530

    1.0

--- Practical 4/Main.java
+++ Practical 4/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main() {
+    public static void main(String[] args) {
         int[][] arr = { {1}, {1, 2}, {1, 2, 3, 4, 5} };
 
         for(int i = 0; i < 3; i++) {
