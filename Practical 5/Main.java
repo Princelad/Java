0c3c9e7c32c410d999a1e0323c5c6e315bd439f8
Princelad <ladprince1112@gmail.com>
2024-06-26 15:57:48 +0530

    1.0

---
+++ Practical 5/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main() {
+        Scanner scan = new Scanner(System.in);
+        
+        
+        scan.close();
+    }
+}
