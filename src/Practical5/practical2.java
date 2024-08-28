30020a9d317928ffd79e7ca034fb14a4f6ed8f2d
Princelad <ladprince1112@gmail.com>
2024-08-28 14:48:42 +0530

    Used Nested try...catch

--- src/Practical5/practical2.java
+++ src/Practical5/practical2.java
@@ -3,38 +3,38 @@ package Practical5;
 public class practical2 {
 
     public static void main(String[] args) {
-        try{
-            int x = 10 / 0;
-        } catch(ArithmeticException e){
-            System.out.println("Invalid division");
-        }
-
-        try{
-            int x = Integer.parseInt("HI");
-        } catch(NumberFormatException e) {
-            System.out.println("Format mismatch");
-        }
-
-        try{
-            String str = args[0];
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Index is invalid");
-        }
-
-        try{
-            String str = args['1'];
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Array index is invalid");
-        }
-
         try {
+            try {
+                int x = 10 / 0;
+            } catch (ArithmeticException e) {
+                System.out.println("Invalid division");
+            }
+
+            try {
+                int x = Integer.parseInt("HI");
+            } catch (NumberFormatException e) {
+                System.out.println("Format mismatch");
+            }
+
+            try {
+                String str = args[0];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Index is invalid");
+            }
+
+            try {
+                String str = args['1'];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Array index is invalid");
+            }
+
+            try {
+                throw new Exception("MyException[" + args[0] + "]");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
             throw new Exception("MyException[" + args[0] + "]");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        try {
-            throw new Exception("MyException[prams]");
         } catch (Exception e) {
             System.out.println("Exception encountered");
         } finally {
